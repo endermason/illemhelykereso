4b4b8eaa6103950e458ba63a698ecc7e6139a435
krichard12 <richardkohegyi@gmail.com>
2022-12-05 21:14:22 +0100

    .env

--- untitled/src/Controller/MapApiController.php
+++ untitled/src/Controller/MapApiController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use App\Repository\ToiletRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MapApiController extends AbstractController
 {
     #[Route('/api/geojson', name: 'app_api_geojson')]
-    public function index(ToiletRepository $repo): JsonResponse
+    public function index(ToiletRepository $repo): Response
     {
         $geojson = array(
             'type'      => 'FeatureCollection',
@@ -35,7 +36,11 @@ class MapApiController extends AbstractController
                 )
             );
             $geojson['features'][] = $feature;
+            $response = new Response(json_encode($geojson));
+            $response->headers->set('Content-Type', 'application/json');
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+
+            return $response;
         }
-        return $this->json($geojson);
     }
 }
